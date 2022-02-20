c08740861aec728d33e04bdca69cf8ffdf336424
outbid <outbid1990@gmail.com>
2022-02-20 20:38:03 +0200

    mux cors added

--- expenses-api/go.mod
+++ expenses-api/go.mod
@@ -1,3 +1,5 @@
 module expenses-api
 
 go 1.17
+
+require github.com/rs/cors v1.8.2
--- expenses-api/main.go
+++ expenses-api/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+	"github.com/rs/cors"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/rates", rates)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", rates)
+
+	// cors.Default() setup the middleware with default options being
+	// all origins accepted with simple methods (GET, POST). See
+	// documentation below for more options.
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8080", handler)
 }
 
 func rates(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(getCurrencies())
 }
